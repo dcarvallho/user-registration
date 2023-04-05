a8dcba7737f19f015c1d34d2165fe937d603a9c0
Davi Augusto <davi_augusto11@hotmail.com>
2023-04-05 14:51:58 -0300

    Added event of listenner in input

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../generic/Card'
 import Button from '../generic/Button'
 
 import styles from './AddUser.module.css'
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
     };
@@ -13,14 +16,24 @@ const AddUser = props => {
         window.alert("You cliked on the button")
     }
 
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+        console.log(enteredUsername, "name")
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+        console.log(enteredAge, "age")
+    }
+
     return(
         <div>
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
+                    <input type="text" id="username" onChange={usernameChangeHandler}/>
                     <label htmlFor="username">Age (Years)</label>
-                    <input type="number" id="username" />
+                    <input type="number" id="age" onChange={ageChangeHandler}/>
                     <Button addNewUser={handleAddUser} type={''}>Add User</Button>
                 </form>
             </Card>
