224e7e8420af80cdd5387f1089b4f22e5145c23f
Davi Augusto <davi_augusto11@hotmail.com>
2023-04-06 16:01:11 -0300

    feature/errorModal: component created of the error modal what show in screen when have some error

---
+++ src/components/generic/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Card from './Card';
+
+const ErrorModal = () => {
+    return (
+        <div>
+            <Card>
+                <header>
+                    <h2></h2>
+                </header>
+                <div>
+                    <p></p>
+                </div>
+                <footer>
+
+                </footer>
+            </Card>
+        </div>
+    )
+};
+
+export default ErrorModal;
